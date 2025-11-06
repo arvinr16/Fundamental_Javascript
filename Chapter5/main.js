4b2a30ce5035899c82a6b5af76a421f6f66c6e53
arvinr16 <arviansyahyk@gmail.com>
2025-11-06 12:56:33 +0700

    Adding continue the progress 'main.js' in Chapter 5 (notFinishYet9)

--- Chapter5/main.js
+++ Chapter5/main.js
@@ -1,2 +1,27 @@
 // Materi Tipe Data dalam Javascript
-// Lanjut
+// 1. 'console.log(typeof 'nama variable');' = Digunakan untuk mengetahui jenis Tipe Data
+
+let dataString = "Paijo";    // Tipe Data = String
+console.log(dataString);
+console.log(typeof dataString);
+
+console.log("")
+
+let dataNumber = 21;        // Tipe Data = Number
+console.log(dataNumber);
+console.log(typeof dataNumber);
+
+console.log("")
+
+let dataBoolean = true;     // Tipe Data = Boolean
+console.log(dataBoolean);
+console.log(typeof dataBoolean);
+
+console.log("")
+
+let dataKosong;             // Tipe Data = Undefined
+console.log(dataKosong);    // Contoh kasus mengganti Tipe Data pada suatu variable
+console.log(typeof dataKosong);
+dataKosong = "Dalijo";
+console.log(dataKosong);
+console.log(typeof dataKosong);
