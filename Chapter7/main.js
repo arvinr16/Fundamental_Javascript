838b99dbd0fd5ab5bf61f52ee103e75140f1a443
arvinr16 <arviansyahyk@gmail.com>
2025-11-20 10:05:42 +0700

    Adding 'main.js' in Chapter 7 (notFinishYet2)

---
+++ Chapter7/main.js
@@ -0,0 +1,16 @@
+// String Operation
+/* 1. Char At
+      Kita bisa mengambil sebagian value atau nilai pada variabel yang kita punya 
+      contoh: */
+let data = "ABCD";
+let dataChar = data.charAt(0); /* 'charAt(numberValue)' digunakan untuk mengambil-
+                                   nilai tertentu dari sebuah nilai pada variabel kita. */
+console.log(`Pengambilan character atau char pada index ke 0: ${dataChar}`);
+dataChar = data.charAt(1);
+console.log(`Pengambilan character atau char pada index ke 1: ${dataChar}`);
+dataChar = data.charAt(2);
+console.log(`Pengambilan character atau char pada index ke 2: ${dataChar}`);
+dataChar = data.charAt(3);
+console.log(`Pengambilan character atau char pada index ke 3: ${dataChar}`);
+
+/* 2.  */
