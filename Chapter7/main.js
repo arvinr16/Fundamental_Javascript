629364583fd339f7488c0627f44a18190ad7df5b
arvinr16 <arviansyahyk@gmail.com>
2025-11-20 13:29:16 +0700

    Continue the progress in Chapter 7 (notFinishYet3)

--- Chapter7/main.js
+++ Chapter7/main.js
@@ -13,4 +13,25 @@ console.log(`Pengambilan character atau char pada index ke 2: ${dataChar}`);
 dataChar = data.charAt(3);
 console.log(`Pengambilan character atau char pada index ke 3: ${dataChar}`);
 
-/* 2.  */
+console.log("")
+
+/* 2. Menyambung String */
+let namaDepan = "Paijo";
+let namaBelakang = "Dalmoyo";
+let namaLengkap = namaDepan.concat(' ',namaBelakang, " yang paling keren");
+/* '.concat' adalah method yang digunakan untuk menggabungkan beberapa string-
+   menjadi satu string baru. */
+console.log(namaLengkap);
+
+console.log("")
+
+/* 3. Mengambil Index 
+      Index yang terambil adalah index pertama-
+      dari suatu nilai jika nilai tersebut lebih dari satu*/
+let namaPanggilan = "PaDalmoyo"
+console.log(namaPanggilan.indexOf('a'));
+/* 'indexOf' digunakan untuk mengambil index pertama atau untuk mencari posisi index di-
+   dalam array atau string */
+
+
+/* 4. Substring */
