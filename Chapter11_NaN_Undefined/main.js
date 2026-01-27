990e52a7fcc938b2e9808d446aff23caaed50145
arvinr16 <arviansyahyk@gmail.com>
2026-01-27 10:46:54 +0700

    Adding materials in 'main.js' Chapter 11 (finish)

--- Chapter11_NaN_Undefined/main.js
+++ Chapter11_NaN_Undefined/main.js
@@ -16,3 +16,30 @@ console.log(`"Paijo" / "Dalijo" :${"Paijo" / "Dalijo"}`);
 console.log(`"Paijo" * "Dalijo" :${"Paijo" * "Dalijo"}`);
 console.log(`"Paijo" + "Dalijo" :${"Paijo" + "Dalijo"}`);
 console.log(`"Paijo" + "Dalijo" :${"Paijo" - "Dalijo"}`);
+let data = parseInt("test123");
+console.log(data);
+/* Jika kita membuat suatu operasi matematika dengan nilai tipe data NaN, maka-
+   hasilnya nanti juga berupa NaN. Atau NaN tidak bisa dioperasikan.
+*/
+
+console.log("");
+
+// 2. Undefined (belum diberi nilai atau belum tahu nilainya)
+let a;
+console.log(`a = ${a}`);
+console.log(Math.sqrt(36));
+console.log(console.log("testing")); /* Bernilai undefined dikarenakan console.log tidak-
+                                        menghasilkan nilai apapun, melainkan dia hanya-
+                                        nge print saja.
+                                     */
+/* Kesimpulan Undefined = Sesuatu yang kita lakukan dan tidak menghasilkan suatu-
+                          nilai apapun, maka akan terjadi Undefined.
+                          Dia bisa jadi sesuatu variabel yang kita tidak-
+                          definisikan nilainya atau tidak punya nilai,-
+                          atau bisa juga sesuatu yang tidak memiliki nilai-
+                          apapun seperti console.log yang hanya nge print saja.
+*/
+let data_1 = console.log("Paijo"); // Ini sebenarnya tidak menghasilkan nilai apapun.
+// berbeda dengan ini:
+let data_2 = Math.cos(3.14);
+console.log(data_2);
