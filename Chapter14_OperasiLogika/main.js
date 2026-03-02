f7553570461ce19cb647bf071578799d8d96a2be
arvinr16 <arviansyahyk@gmail.com>
2026-03-02 09:00:00 +0700

    Adding materials in 'main.js' Chapter 14 (notFinishYet6)

---
+++ Chapter14_OperasiLogika/main.js
@@ -0,0 +1,6 @@
+// Operator Logika
+/* Javascript memiliki 3 Operator Logika utama, yaitu:
+   1. '&&'(AND) = Menghasilkan True hanya jika kedua nilai bernilai True.
+   2. '||'(OR)  = Menghasilkan True jika minimal satu nilai bernilai True.
+   3. '!'(NOT)  = Membalik nilai. Jika True jadi False, jika False jadi True.
+*/
