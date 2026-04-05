07439fde962ca2ee5c1f8dabf655de26b610dcb9
arvinr16 <arviansyahyk@gmail.com>
2026-04-05 19:29:26 +0700

    Adding 'main.js' in Chapter 15 (notFinishYet2)

---
+++ Chapter15_OperasiKomparasi/main.js
@@ -0,0 +1,20 @@
+// Operasi Komparasi
+/* Operasi Komparasi pada dasarnya adalah membandingkan dua buah nilai-
+   dan selalu mengembalikan nilai boolean True atau False.
+*/
+
+/* 1. '=='(Loose equality) = Membandingkan dua nilai dengan mengabaikan perbedaan-
+                             tipe data.
+*/
+
+/* 2. '!='(Loose inequality) = Mengecek apakah dua nilai tidak sama, namun tetap-
+                               mengabaikan perbedaan tipe data.
+*/
+
+/* 3. '==='(Strict equality) = Membandingkan dua nilai sekaligus tipe datanya.
+                               (Direkomendasikan untuk menghindari bug tidak terduga)
+*/
+
+/* 4. '!=='(Strict inequality) = Mengecek apakah dua nilai tidak sama dengan-
+                                 mempertimbangkan tipe datanya juga.
+*/
