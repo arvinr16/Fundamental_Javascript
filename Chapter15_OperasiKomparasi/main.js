cf88e8bc47ea5183f2c55abbf321f11208e9e63d
arvinr16 <arviansyahyk@gmail.com>
2026-04-05 19:47:05 +0700

    Adding 'main.js' in Chapter 15 (notFinishYet3)

--- Chapter15_OperasiKomparasi/main.js
+++ Chapter15_OperasiKomparasi/main.js
@@ -3,11 +3,11 @@
    dan selalu mengembalikan nilai boolean True atau False.
 */
 
-/* 1. '=='(Loose equality) = Membandingkan dua nilai dengan mengabaikan perbedaan-
+/* 1. '=='(Equality) = Membandingkan dua nilai dengan mengabaikan perbedaan-
                              tipe data.
 */
 
-/* 2. '!='(Loose inequality) = Mengecek apakah dua nilai tidak sama, namun tetap-
+/* 2. '!='(Inequality) = Mengecek apakah dua nilai tidak sama, namun tetap-
                                mengabaikan perbedaan tipe data.
 */
 
@@ -17,4 +17,20 @@
 
 /* 4. '!=='(Strict inequality) = Mengecek apakah dua nilai tidak sama dengan-
                                  mempertimbangkan tipe datanya juga.
+*/
+
+/* 5. '>'(Greater than/lebih besar dari) = Mengecek apakah nilai di kiri lebih besar-
+                                           dari nilai di kanan.
+*/
+
+/* 6. '<'(Less than/lebih kecil dari) = Mengecek apakah nilai di kiri lebih kecil-
+                                        dari nilai di kanan.
+*/
+
+/* 7. '>='(Greater or equal/lebih besar sama dengan) = Mengecek apakah nilai di kiri lebih-
+                                                       besar dari atau sama dengan nilai di kanan.
+*/
+
+/* 8. '<='(Less or equal/lebih kecil sama dengan) = Mengecek apakah nilai di kiri lebih-
+                                                    kecil dari atau sama dengan nilai di kanan.
 */
