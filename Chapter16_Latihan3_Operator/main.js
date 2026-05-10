300f8ecaed53bb2f3653bce1470fff9625f837f9
arvinr16 <arviansyahyk@gmail.com>
2026-05-10 10:00:00 +0700

    Adding materials in 'main.js' Chapter 16 (notFinishYet9)

--- Chapter16_Latihan3_Operator/main.js
+++ Chapter16_Latihan3_Operator/main.js
@@ -1,1 +1,23 @@
-console.log("Testing");
+let aritmatikaAngka1, operatorAritmatika, aritmatikaAngka2, hasilAritmatika;
+document.getElementById("buttonAritmatika").onclick = function () {
+  //input:
+  aritmatikaAngka1 = document.getElementById("aritmatikaAngka1").value;
+  aritmatikaAngka2 = document.getElementById("aritmatikaAngka2").valueAsNumber;
+  operatorAritmatika = document.getElementById("operatorAritmatika").value;
+  // console.log(aritmatikaAngka1);
+  // console.log(operatorAritmatika);
+  // console.log(aritmatikaAngka2);
+
+  //operasi:
+  hasilAritmatika = eval(aritmatikaAngka1 + operatorAritmatika + aritmatikaAngka2);   
+ /* eval("10+10");  Fungsi 'eval' adalah mengubah string menjadi sebuah kode-
+                    Javascipt. Tetapi penggunaan 'eval' tidak disarankan-
+                    dikarenakan dia mempunyai kerentanan terhadap serangan-
+                    cybersecurity XSS(Cross Site Scripting). Jadi alangkah-
+                    lebih baik kita menggunakan function 'JSON.parse'.   */
+  /* let user = JSON.parse(`{"nama": "John"}`); Disarankan menggunakan 'JSON.parse' seperti itu
+     console.log(user); */
+
+  //output:
+  document.getElementById("hasilAritmatika").textContent = hasilAritmatika;
+};
